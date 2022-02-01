7ac24392b50041e05a553cfa3d5b7a12e9a8ab0e
Fabian Kägy <mail@fabian-kaegy.de>
2022-02-01 16:10:58 +0100

    initial setup

---
+++ plugin.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Plugin Name:       Block Performance Testing
+ * Description:       Example block written with ESNext standard and JSX support – build step required.
+ * Requires at least: 5.8
+ * Requires PHP:      7.0
+ * Version:           0.1.0
+ * Author:            The WordPress Contributors
+ * License:           GPL-2.0-or-later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       block-performance-testing
+ *
+ * @package           block-performance-testing
+ */
+
+/**
+ * Registers the block using the metadata loaded from the `block.json` file.
+ * Behind the scenes, it registers also all assets so they can be enqueued
+ * through the block editor in the corresponding context.
+ *
+ * @see https://developer.wordpress.org/reference/functions/register_block_type/
+ */
+function block_performance_testing__block_init() {
+	register_block_type( __DIR__ . '/build' );
+}
+add_action( 'init', 'block_performance_testing__block_init' );
+
+add_filter( 'should_load_separate_core_block_assets', '__return_true' );
+
+add_filter(
+	'styles_inline_size_limit',
+	function() {
+		return 50000; // Size in bytes.
+	}
+);
